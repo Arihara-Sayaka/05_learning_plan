e7af8ab909a83db74bf1cee7b935202a1add6a9d
Arihara-Sayaka <1803.arinko@gmail.com>
2020-02-06 00:00:00 +0900

    first commit

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
 <?php
+
+define('DSN', 'mysql:host=mysql;dbname=learning_plan;charset=utf8;');
+define('USER', 'admin_user');
+define('PASSWORD', '1234');
+
+try {
+  $dbh = new PDO(DSN, USER, PASSWORD);
+} catch (PDOException $e) {
+  echo $e->getMessage();
+  exit;
+}
+

8ee51e5c614e9f8843f65a91dbee96aa9e108782
Arihara-Sayaka <1803.arinko@gmail.com>
2020-02-08 12:52:21 +0900

    fix

--- index.php
+++ index.php
@@ -1,15 +1,7 @@
 <?php
 
-define('DSN', 'mysql:host=mysql;dbname=learning_plan;charset=utf8;');
-define('DB_USER', 'admin_user');
-define('DB_PASSWORD', '1234');
+//ファイルの読み込み
+require_once('config.php');
+require_once('function.php');
 
-function connectDb() {
-  try {
-    return new PDO(DSN, DB_USER, DB_PASSWORD);
-  } catch (PDOException $e) {
-    echo $e->getMessage();
-    exit;
-  }
-}
-
+$dbh = connectDb();
